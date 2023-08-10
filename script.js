b61f65fd758d4cddb805bc2fdcc4af712a014443
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-10 11:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-//your JS code here. If required.
-  const apiKey = '117a142046bf80b24ded8dbab6fbedd3'; // Replace with your actual API key
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+const weatherDataDiv = document.getElementById("weatherData");
 
-        // Function to fetch weather data
-        function getWeatherData() {
-            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={117a142046bf80b24ded8dbab6fbedd3}`;
-            
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    const weatherDescription = data.weather[0].description;
-                    const cityName = data.name;
-                    const weatherInfo = `Current weather in ${cityName}: ${weatherDescription}`;
-                    
-                    document.getElementById('weatherData').textContent = weatherInfo;
-                })
-                .catch(error => {
-                    console.error('Error fetching weather data:', error);
-                });
-        }
-
-        // Add click event listener to the button
-        const getWeatherButton = document.getElementById('getWeatherButton');
-        getWeatherButton.addEventListener('click', getWeatherData);
+getWeatherBtn.addEventListener("click", () => {
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={4929ccc13d3c700c550ba9093c80cd28}";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherDescription = data.weather[0].description;
+            weatherDataDiv.textContent = `Current weather in London: ${weatherDescription}`;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+});
